86748b07b611c11c3b27d1805ff02fe50bd23421
Sagar <sagar.kapoor@vyld.io>
2024-07-08 18:02:06 +0530

    added white

--- ttol/src/App.js
+++ ttol/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LieRectangel from './components/LieRectangel';
+import WhiteRectangle from './components/WhiteRectangle';
 
 function App() {
   return (
    <div>
 <LieRectangel/>
+<WhiteRectangle/>
     </div>
   );
 }

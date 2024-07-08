712feb95d138f1ea4e8f39cdeb9dcd4aa356e6d1
Sagar <sagar.kapoor@vyld.io>
2024-07-08 18:31:06 +0530

    done with 5 card components

--- ttol/src/App.js
+++ ttol/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import LieRectangel from './components/LieRectangel';
 import WhiteRectangle from './components/WhiteRectangle';
+import PinkReactangle from './components/PinkReactangle';
+import RedRectangel from './components/RedRectangle';
 
 function App() {
   return (
    <div>
 <LieRectangel/>
 <WhiteRectangle/>
+<PinkReactangle/>
+<RedRectangel/>
     </div>
   );
 }

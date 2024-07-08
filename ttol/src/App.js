d8de806993171295be001ba8d2458f9bd58db5aa
Sagar <sagar.kapoor@vyld.io>
2024-07-08 17:39:57 +0530

    done

---
+++ ttol/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LieRectangel from './components/LieRectangel';
+
+function App() {
+  return (
+   <div>
+<LieRectangel/>
+    </div>
+  );
+}
+
+export default App;
